37ed2cec3172a1ca3412e4289b2c17a632f0e646
fedesilva <federico.silva@flyzipline.com>
2025-02-13 10:30:31 -0800

    add json and jsonc to tree sitter required

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,7 +17,7 @@ require("config.options")
 
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "scala" },
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "scala", "json", "jsonc" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
