0379f9874061a2330568184a9027e605e6708216
federico silva <fedesilva@users.noreply.github.com>
2025-02-23 16:13:33 -0800

    apply neotree migrations advice

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -5,11 +5,14 @@ return {
     require("neo-tree").setup({
       close_if_last_window = true, -- Optional: Close Neotree if it's the last window
       filesystem = {
-        follow_current_file = true, -- Auto-update Neotree when switching buffers
+        follow_current_file = {
+          enabled = true, -- Auto-update Neotree when switching buffers
+        }
       },
       sources = { "filesystem", "buffers" }, -- Enable buffers in NeoTree
       buffers = {
-        follow_current_file = true, -- Auto-update NeoTree when switching buffers
+        -- Auto-update NeoTree when switching buffers
+        follow_current_file = { enabled = true }
       },
     })
 
@@ -18,10 +21,9 @@ return {
       callback = function()
         local win_id = vim.fn.bufwinid("Neotree") -- Check if Neotree is open
         if win_id ~= -1 then
-          vim.cmd("Neotree reveal") -- Run Neotree reveal if it's open
+          vim.cmd("Neotree reveal")               -- Run Neotree reveal if it's open
         end
       end,
     })
   end,
 }
-
