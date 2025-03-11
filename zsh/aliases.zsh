bafa969cf2ce758e8d95b9ac97d2185e5c05da29
fedesilva <federico.silva@flyzipline.com>
2025-03-11 09:46:02 -0700

    zsh aliases

---
+++ zsh/aliases.zsh
@@ -0,0 +1,48 @@
+#vim: set filetype=sh
+#echo "Custom aliases"
+
+#
+# ls aliases enable color support of ls and also add handy aliases
+#
+alias ls='ls -G'
+alias ll="ls -lh"
+alias la="ls -lah"
+alias l="ls"
+
+
+#  Homebrew
+#
+
+alias br=brew
+alias brs="br search"
+alias bri="br info"
+alias brh="br home"
+
+#
+#  Make Neovim the default vim
+#
+alias vi=nvim 
+alias vim=nvim
+alias mvim=vimr
+
+
+alias tar=gnutar
+
+#
+#  GIT aliases
+#
+alias gpom="git push origin master:refs/heads/master"
+alias gs="git status"
+alias gadd="git add"
+alias gd="git diff"
+alias gci="git commit -v"
+alias gca="git commit -av"
+alias gcl="git log --grap"
+alias gl="git lola"
+alias grs="git remote show"
+
+alias gw="git whatchanged"
+alias gwp="git whatchanged -p"
+
+
+
